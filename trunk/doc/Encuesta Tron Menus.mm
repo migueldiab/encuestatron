98c54680a53638f68e9e10bd24f3cbafa4d605e4
migueldiab <migueldiab@582d2490-ccaf-11de-9e82-f790775585aa>
2009-11-08 21:47:17 +0000

    inicio proyecto

---
+++ trunk/doc/Encuesta Tron Menus.mm
@@ -0,0 +1,82 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1257635166477" ID="ID_1244220128" MODIFIED="1257639787301" STYLE="fork" TEXT="Encuesta Tron">
+<font NAME="SansSerif" SIZE="23"/>
+<node CREATED="1257635824570" HGAP="44" ID="ID_1275019200" MODIFIED="1257641016367" POSITION="right" STYLE="bubble" TEXT="https backend" VSHIFT="-45">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="password"/>
+<node CREATED="1257635842767" HGAP="48" ID="ID_1082329072" MODIFIED="1257636417560" VSHIFT="-23">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Funcionarios
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="fema"/>
+<node CREATED="1257636071565" ID="ID_133148896" MODIFIED="1257640683938" STYLE="bubble" TEXT="Administrador">
+<icon BUILTIN="male2"/>
+</node>
+<node CREATED="1257636078234" HGAP="30" ID="ID_405906678" MODIFIED="1257640684905" TEXT="Agente" VSHIFT="40">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node CREATED="1257635865231" HGAP="64" ID="ID_1062902207" MODIFIED="1257640720138" STYLE="bubble" TEXT="Clientes" VSHIFT="-56">
+<icon BUILTIN="female2"/>
+</node>
+<node CREATED="1257640240708" HGAP="41" ID="ID_1357261536" MODIFIED="1257640727174" TEXT="CRUD Usuarios" VSHIFT="27">
+<icon BUILTIN="male2"/>
+</node>
+<node CREATED="1257640256488" ID="ID_178598471" MODIFIED="1257640262090" TEXT="Listados">
+<node CREATED="1257640263141" ID="ID_777939418" MODIFIED="1257640270771" TEXT="Encuestas">
+<node CREATED="1257640271729" ID="ID_327345571" MODIFIED="1257640759310" TEXT="por Agente">
+<icon BUILTIN="male2"/>
+</node>
+<node CREATED="1257640281237" ID="ID_1366737115" MODIFIED="1257640744100" TEXT="por Cliente">
+<icon BUILTIN="male1"/>
+<icon BUILTIN="male2"/>
+</node>
+<node CREATED="1257640283990" ID="ID_416522486" MODIFIED="1257640291761" TEXT="por Fechas">
+<node CREATED="1257640292438" ID="ID_417905924" MODIFIED="1257640786906" TEXT="Ingreso">
+<icon BUILTIN="male1"/>
+<icon BUILTIN="male2"/>
+<icon BUILTIN="female2"/>
+</node>
+<node CREATED="1257640296377" ID="ID_409600461" MODIFIED="1257640788997" TEXT="Vigencia">
+<icon BUILTIN="male1"/>
+<icon BUILTIN="male2"/>
+<icon BUILTIN="female2"/>
+</node>
+<node CREATED="1257640300534" ID="ID_1327722334" MODIFIED="1257640791181" TEXT="Cierre">
+<icon BUILTIN="male1"/>
+<icon BUILTIN="male2"/>
+<icon BUILTIN="female2"/>
+</node>
+</node>
+<node CREATED="1257640808503" ID="ID_1296122647" MODIFIED="1257640813902" TEXT="Resultados">
+<icon BUILTIN="female2"/>
+</node>
+</node>
+</node>
+<node CREATED="1257640568722" ID="ID_718695357" MODIFIED="1257640731823" TEXT="CRUD Encuestas">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node CREATED="1257635872524" HGAP="-7" ID="ID_1983808234" MODIFIED="1257640980651" POSITION="left" STYLE="bubble" TEXT="http frontend" VSHIFT="86">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1257635878975" HGAP="21" ID="ID_883176157" MODIFIED="1257640982382" TEXT="publico" VSHIFT="36">
+<icon BUILTIN="family"/>
+<node CREATED="1257640983153" HGAP="35" ID="ID_1932313172" MODIFIED="1257641004183" TEXT="Respuesta de encuesta" VSHIFT="29"/>
+</node>
+</node>
+<node CREATED="1257640867447" HGAP="-89" ID="ID_1790506337" MODIFIED="1257641013294" POSITION="right" STYLE="bubble" TEXT="WebService" VSHIFT="21">
+<icon BUILTIN="password"/>
+<node CREATED="1257640891331" HGAP="19" ID="ID_166088868" MODIFIED="1257641585215" TEXT="Respuesta de encuesta" VSHIFT="-26"/>
+<node CREATED="1257640903351" ID="ID_1797295588" MODIFIED="1257641583889" TEXT="Consulta de Resultados" VSHIFT="25"/>
+</node>
+</node>
+</map>
