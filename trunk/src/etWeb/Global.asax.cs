7fcd5ba934a5bd37d73aa96250a47fee2359ed6f
migueldiab <migueldiab@582d2490-ccaf-11de-9e82-f790775585aa>
2009-12-05 23:24:22 +0000

    Mas Doc y redist

--- trunk/src/etWeb/Global.asax.cs
+++ trunk/src/etWeb/Global.asax.cs
@@ -12,16 +12,16 @@ namespace etWeb
 
     public class MvcApplication : System.Web.HttpApplication
     {
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 "Default",                                              // Route name
-                "{controller}/{action}/{id}",                           // URL with parameters
-                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+                "sampleasp/{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = ""}    // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
